426bf9b3a0f828cd894eb390f0d8890e1069cf07
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-01-14 15:02:52 -0800

    Add bench.py

---
+++ bench.py
@@ -0,0 +1,21 @@
+import os
+
+import duckdb
+import pyarrow.dataset as ds
+
+if __name__ == "__main__":
+    lineitem = ds.dataset(os.path.join("/mnt/cephfs/tpch_parquet", "lineitem.parquet"), format="parquet")
+    supplier = ds.dataset(os.path.join("/mnt/cephfs/tpch_parquet", "supplier.parquet"), format="parquet")
+    customer = ds.dataset(os.path.join("/mnt/cephfs/tpch_parquet", "customer.parquet"), format="parquet")
+    region   = ds.dataset(os.path.join("/mnt/cephfs/tpch_parquet", "region.parquet"), format="parquet")
+    nation   = ds.dataset(os.path.join("/mnt/cephfs/tpch_parquet", "nation.parquet"), format="parquet")
+    orders   = ds.dataset(os.path.join("/mnt/cephfs/tpch_parquet", "orders.parquet"), format="parquet")
+    part     = ds.dataset(os.path.join("/mnt/cephfs/tpch_parquet", "part.parquet"), format="parquet")
+
+    conn = duckdb.connect()
+
+    query = conn.execute("SELECT * FROM lineitem")
+
+    record_batch_reader = query.fetch_record_batch()
+    chunk = record_batch_reader.read_next_batch()
+    print(chunk.to_pandas())
