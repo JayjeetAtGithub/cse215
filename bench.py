5d8b54af90f36dbca0352b563515cb54ae497ad5
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-01-14 18:52:51 -0800

    try query 1

--- bench.py
+++ bench.py
@@ -14,7 +14,28 @@ if __name__ == "__main__":
 
     conn = duckdb.connect()
 
-    query = conn.execute("SELECT * FROM lineitem")
+    query = conn.execute("""
+SELECT
+    l_returnflag,
+    l_linestatus,
+    sum(l_quantity) AS sum_qty,
+    sum(l_extendedprice) AS sum_base_price,
+    sum(l_extendedprice * (1 - l_discount)) AS sum_disc_price,
+    sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) AS sum_charge,
+    avg(l_quantity) AS avg_qty,
+    avg(l_extendedprice) AS avg_price,
+    avg(l_discount) AS avg_disc,
+    count(*) AS count_order
+FROM
+    lineitem
+WHERE
+    l_shipdate <= CAST('1998-09-02' AS date)
+GROUP BY
+    l_returnflag,
+    l_linestatus
+ORDER BY
+    l_returnflag,
+    l_linestatus;""")
 
     record_batch_reader = query.fetch_record_batch()
     chunk = record_batch_reader.read_next_batch()
