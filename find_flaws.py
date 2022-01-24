a96e1092c7bd502668e5a64273f0ca024ccf8ec3
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-01-23 21:44:44 -0800

    Add script to find bad files

---
+++ find_flaws.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+
+if __name__ == "__main__":
+    dataset_dir = str(sys.argv[1])
+    for root, dirs, files in os.walk(dataset_dir):
+        for file in files:
+            if file.endswith(".parquet"):
+                file_path = os.path.join(root, file)
+                file_size = os.stat(file_path).st_size/(1024*1024)
+
+                if file_size > 16:
+                    print(f"NOT OK! {file_path} = {file_size} MB")
+                else:
+                    print(f"OK ! {file_path} = {file_size} MB")
