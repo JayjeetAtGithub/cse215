e1f444b97771ba0d2682bec68ff2209a5b0a3b0b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-02-06 10:02:40 -0800

    Add initial results

---
+++ plot_results.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import json
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def clean_string(path):
+    path = path.replace(".", "_")
+    path = path.replace("/", "_")
+    return path
+
+
+if __name__ == "__main__":
+    result_dir = str(sys.argv[1])    
+    queries = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20, 22]
+    data = list()
+
+    for query in queries:
+        parquet_path = os.path.join(result_dir, "bench_result." + str(query) + ".parquet.json")
+        skyhook_path = os.path.join(result_dir, "bench_result." + str(query) + ".skyhook.json")
+
+        with open(parquet_path) as f:
+            parquet_result = json.load(f)
+
+        with open(skyhook_path) as f:
+            skyhook_result = json.load(f)
+        
+        for point in parquet_result:
+            data.append(point)
+        for point in skyhook_result:
+            data.append(point)
+
+    df = pd.DataFrame(data) 
+    print(df)
+
+    sns.barplot(data=df, x="query", y="latency", hue="format")
+    plt.savefig("plot.pdf")
