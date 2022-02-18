c67835a56923d51bc13d16186bacb68f1d538c9d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-02-17 16:00:38 -0800

    Updates

---
+++ test_equal.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import time
+
+import multiprocessing as mp
+
+import duckdb
+import pyarrow.dataset as ds
+
+
+def drop_caches():
+    os.system("sync")
+    os.system("echo 3 > /proc/sys/vm/drop_caches")
+    os.system("sync")
+    time.sleep(2)
+
+
+if __name__ == "__main__":
+    dataset_path = str(sys.argv[1])
+    query_no = int(sys.argv[2])
+
+    format="parquet"
+    lineitem = ds.dataset(os.path.join(dataset_path, "lineitem"), format=format)
+    supplier = ds.dataset(os.path.join(dataset_path, "supplier"), format=format)
+    customer = ds.dataset(os.path.join(dataset_path, "customer"), format=format)
+    region   = ds.dataset(os.path.join(dataset_path, "region"), format=format)
+    nation   = ds.dataset(os.path.join(dataset_path, "nation"), format=format)
+    orders   = ds.dataset(os.path.join(dataset_path, "orders"), format=format)
+    part     = ds.dataset(os.path.join(dataset_path, "part"), format=format)
+    partsupp = ds.dataset(os.path.join(dataset_path, "partsupp"), format=format)
+
+    with open(f"queries/q{query_no}.sql", "r") as f:
+        query = f.read()
+
+    query = f"PRAGMA threads={mp.cpu_count()};\n{query}"
+    drop_caches()
+    conn = duckdb.connect()
+    result_parquet = conn.execute(query).fetchdf()
+    conn.close()
+
+    format="skyhook"
+    lineitem = ds.dataset(os.path.join(dataset_path, "lineitem"), format=format)
+    supplier = ds.dataset(os.path.join(dataset_path, "supplier"), format=format)
+    customer = ds.dataset(os.path.join(dataset_path, "customer"), format=format)
+    region   = ds.dataset(os.path.join(dataset_path, "region"), format=format)
+    nation   = ds.dataset(os.path.join(dataset_path, "nation"), format=format)
+    orders   = ds.dataset(os.path.join(dataset_path, "orders"), format=format)
+    part     = ds.dataset(os.path.join(dataset_path, "part"), format=format)
+    partsupp = ds.dataset(os.path.join(dataset_path, "partsupp"), format=format)
+
+    with open(f"queries/q{query_no}.sql", "r") as f:
+        query = f.read()
+
+    query = f"PRAGMA threads={mp.cpu_count()};\n{query}"
+    drop_caches()
+    conn = duckdb.connect()
+    result_skyhook = conn.execute(query).fetchdf()
+    conn.close()
+
+    print(result_parquet.equals(result_skyhook))
