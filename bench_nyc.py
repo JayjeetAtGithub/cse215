24229cbde23935d276698ea75fd4a8f49dba1fe8
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-03-12 12:29:58 -0800

    Update nyc taxi data experiment script

--- bench_nyc.py
+++ bench_nyc.py
@@ -17,12 +17,7 @@ def drop_caches():
 
 
 if __name__ == "__main__":
-    format = str(sys.argv[1])
-
-    data = list()
-    dataset_ = ds.dataset("/mnt/cephfs/dataset", format=format)
     selectivity = [1, 10, 25, 50, 75, 90, 99, 100] 
-
     queries = {
         "1": "SELECT * FROM dataset_ WHERE total_amount > 69",
         "10": "SELECT * FROM dataset_ WHERE total_amount > 27",
@@ -34,25 +29,28 @@ if __name__ == "__main__":
         "100": "SELECT * FROM dataset_",
     }
 
-    for sel in selectivity:
-        for _ in range(5):
-            drop_caches()
-            conn = duckdb.connect()
-            s = time.time()
-            base_query = f"PRAGMA disable_object_cache;\nPRAGMA threads={mp.cpu_count()};\n{queries[str(sel)]}"
-            result = conn.execute(base_query).fetchall()
-            e = time.time()
-            conn.close()
+    for fmt in ["parquet", "skyhook"]:
+        dataset_ = ds.dataset("/mnt/cephfs/dataset", format=fmt)
+        for sel in selectivity:
+            data = list()
+            for _ in range(5):
+                drop_caches()
+                conn = duckdb.connect()
+                s = time.time()
+                base_query = f"PRAGMA disable_object_cache;\nPRAGMA threads={mp.cpu_count()};\n{queries[str(sel)]}"
+                result = conn.execute(base_query).fetchall()
+                e = time.time()
+                conn.close()
 
-            log_str = f"{sel}|{format}|{e - s}"
-            print(log_str)
+                log_str = f"{sel}|{fmt}|{e - s}"
+                print(log_str)
 
-            data.append({
-                "query": sel,
-                "format": format,
-                "latency": e - s
-            })
+                data.append({
+                    "query": sel,
+                    "format": fmt,
+                    "latency": e - s
+                })
 
-        with open(f"nyctaxi_results/bench_result.{sel}.{format}.json", "w") as f:
-            f.write(json.dumps(data))
+            with open(f"nyctaxi_results/bench_result.{sel}.{fmt}.json", "w") as f:
+                f.write(json.dumps(data))
     print("Benchmark finished")
